80cd04334e9e2e9830f58f820226e8a9496590c1
Tyler Gabb <tylerjgabb@gmail.com>
2024-02-22 20:26:41 -0700

    add a non tylerjgabb dependency

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	lib "github.com/TylerJGabb/my-go-module/lib"
 	"github.com/TylerJGabb/new-go-priv-mod/priv"
+	gin "github.com/gin-gonic/gin"
 )
 
 func main() {
 	lib.Hello()
 	priv.PrivateFunc()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 }
